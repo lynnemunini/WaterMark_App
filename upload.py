1879be28a47d67c41b2d589fd0e437711125d763
lynnemunini <muninilynne65@gmail.com>
2022-02-27 23:32:15 +0300

    Added label to select file

--- upload.py
+++ upload.py
@@ -7,18 +7,23 @@ root = Tk()
 root.title('Upload Image')
 root.resizable(True, True)
 
-filetypes = (
-        ('png files', '*.png'),
-        ('all files', '*.*')
-    )
-root.filename = fd.askopenfilename(
-        title='Open a file',
-        initialdir='/home/lynne/Pictures',
-        filetypes=filetypes)
 
-# my_label = Label(root, text=root.filename).pack()
-my_image = ImageTk.PhotoImage(Image.open(root.filename))
-my_image_label = Label(image=my_image).pack()
+def open():
+        global my_image
+        filetypes = (
+                ('png files', '*.png'),
+                ('all files', '*.*')
+        )
+        root.filename = fd.askopenfilename(
+                title='Open a file',
+                initialdir='/home/lynne/Pictures',
+                filetypes=filetypes)
+
+        # my_label = Label(root, text=root.filename).pack()
+        my_image = ImageTk.PhotoImage(Image.open(root.filename))
+        my_image_label = Label(image=my_image).pack()
+
+btn = Button(root, text="Select File", command=open).pack()
 
 # run the application
 root.mainloop()
