22d0a427ab8c5547524e3bfdb4e5fbc835597bbf
lynnemunini <muninilynne65@gmail.com>
2022-03-01 19:55:38 +0300

    Added save and change image buttons

--- upload.py
+++ upload.py
@@ -6,12 +6,12 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title('Upload Image')
 root.resizable(True, True)
+root.geometry("1000x800")
+root.configure(bg='white') 
 
 
-
-def open():
-        global my_image
-        
+def get_image():
+        global my_image        
         filetypes = (
                 ('png files', '*.png'),
                 ('all files', '*.*')
@@ -23,15 +23,33 @@ def open():
 
         my_image = Image.open(root.filename)
         h = my_image.height
-        print(h)
         w = my_image.width
-        print(w)
         my_image = my_image.resize((int(w/2), int(h/2)), Image.ANTIALIAS)
         my_image = ImageTk.PhotoImage(my_image)
-        my_image_label = Label(image=my_image).pack()        
 
-btn = Button(root, text="Select File", command=open).pack()
+        change_btn = Button(text='Change Image', command=lambda: change_image())
+        save_btn = Button(text='Save Image')
+
+        image_canvas = Canvas(width=1000, height=600, bg="white", highlightthickness=0)
+        image_canvas.create_image(500, 350, image=my_image)
+        image_canvas.place(x=0, y=50)
+        change_btn.place(x=300, y=700) 
+        save_btn.place(x=600, y=700)
+
+        
+
+def open():
+        btn.destroy()
+        get_image()   
+
+def change_image():
+        get_image()
+
+        
+btn = Button(root, text="Select File", command=lambda: open())
+btn.place(x=450, y=400)
 
 
 # run the application
 root.mainloop()
+
