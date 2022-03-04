31f3745e50bab3f1982ba4eacbc71a1239ca73b4
lynnemunini <muninilynne65@gmail.com>
2022-03-04 22:35:54 +0300

    Added resize image

--- upload.py
+++ upload.py
@@ -7,8 +7,12 @@ import os
 root = Tk()
 root.title('Upload Image')
 root.resizable(False, False)
-root.geometry("1000x800")
+root.geometry("800x800")
 root.configure(bg='white') 
+
+# get the screen size
+scr_width = root.winfo_screenwidth()
+scr_height = root.winfo_screenheight()
 
 
 def get_image():      
@@ -23,20 +27,21 @@ def get_image():
 
         # Picture name 
         pic_name = root.filename.split('/')[-1]
-
         my_image = Image.open(root.filename)
+        if my_image.width > scr_width or my_image.height > scr_height:
+                # only resize image bigger than the screen
+                ratio = min(scr_width/my_image.width, scr_height/my_image.height)
+                my_image = my_image.resize((int(my_image.width*ratio), int(my_image.height*ratio)))
         # h = my_image.height
         # w = my_image.width
         # my_image = my_image.resize((int(w/2), int(h/2)), Image.ANTIALIAS)
-        my_image = ImageTk.PhotoImage(my_image)
-
-        
+        my_image = ImageTk.PhotoImage(my_image)  
 
 
         #Image Canvas
-        image_canvas = Canvas(width=1000, height=600, bg="white", highlightthickness=0)
-        image_canvas.create_image(500, 350, image=my_image)
-        image_canvas.place(x=0, y=50)
+        image_canvas = Canvas(width=800, height=600, bg="white", highlightthickness=0)
+        image_canvas.create_image(400, 300, image=my_image)
+        image_canvas.place(x=0, y=0)
 
         change_btn = Button(text='Change Image', command=lambda: change_image(image_canvas))
         save_btn = Button(text='Save', command=lambda: save(my_image, pic_name))
@@ -60,7 +65,7 @@ def save(image, name):
 
         
 btn = Button(root, text="Select File", command=lambda: open())
-btn.place(x=450, y=400)
+btn.place(x=400, y=400)
 
 
 # run the application
